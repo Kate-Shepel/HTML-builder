262cc7da7fa2e2b00c2598317bcd767587f93dbc
Kate-Shepel <kateborichevskaya@gmail.com>
2023-05-07 13:45:12 +0200

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const { copyFile, readdir } = require('fs/promises');
+const pathToDestFolder = path.join(__dirname, 'files-copy');
+const pathToSrcFolder = path.join(__dirname, 'files');
+
+async function createFolder() {
+  await fs.promises.mkdir(pathToDestFolder);
+  // console.log('Folder is created');
+}
+
+async function deleteFolder() {
+  await fs.promises.rm(pathToDestFolder, { recursive:true, force: true });
+  // console.log('Folder is deleted');
+}
+
+async function copyFilesToDestFolder() {
+  try {
+    const foldersList = await readdir(path.dirname(__filename));
+    // console.log('foldersList:', foldersList);
+    if (foldersList.includes('files-copy')) {
+      await deleteFolder();
+      copyFilesToDestFolder();
+    } else {
+      await createFolder();
+      const filesList = await readdir(pathToSrcFolder);
+      // console.log('filesList:', filesList);
+  
+      for (const file of filesList) {
+        await copyFile(path.join(pathToSrcFolder, `${file}`), path.join(pathToDestFolder, `${file}`));
+      }
+      console.log('The files have been successfully copied to a new folder');
+    }
+  } catch (error) {
+    console.error('error', error);
+  }
+}
+
+copyFilesToDestFolder();
 
